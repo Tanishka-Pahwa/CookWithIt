4dbe966f1931436c1dc89294204d8c6da6affe01
Tanishka-Pahwa <131804973+Tanishka-Pahwa@users.noreply.github.com>
2023-10-29 19:35:37 +0530

    Add files via upload

---
+++ users/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from .models import Profile
+
+admin.site.register(Profile)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ users/urls.py
@@ -0,0 +1,4 @@
+# posts/urls.py
+from django.urls import path
+from . import views
+
